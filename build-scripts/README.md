b1a1cfb973cee752b8c0aac01e7b53aa52c50ecb
Adam Wilson <adam@codegarden.co.uk>
2015-03-30 23:49:12 +0100

    add readme

---
+++ build-scripts/README.md
@@ -0,0 +1,9 @@
+## Aubio build scripts for Android 
+
+These scripts were put together with the help of Paul Brossier the author of Aubio. 
+
+They were used to create the static library files found in this repository. 
+
+You may need to tweak folder locations depending on where you have things on your setup. 
+
+
---
+++ build-scripts/android-build-armv7-a.sh
@@ -0,0 +1,18 @@
+#! /bin/sh
+
+"""
+build the toold chain home with:
+build/tools/make-standalone-toolchain.sh --platform=android-9 --install-dir=../android-9-toolchain
+"""
+
+NDK_HOME=~/dev/SDKs/android-9-toolchain-4.9
+set -x
+set -e
+export CC=$NDK_HOME/bin/arm-linux-androideabi-gcc
+export CFLAGS="-I$NDK_HOME/include -march=armv7-a -mfloat-abi=softfp"
+export LDFLAGS="-L$NDK_HOME/lib"
+export AR=$NDK_HOME/bin/arm-linux-androideabi-ar
+./waf distclean
+./waf configure --with-target-platform android --disable-jack
+./waf build --verbose
+./waf install --destdir dist-android/armv7-a
